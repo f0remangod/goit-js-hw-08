f8db8fa95a5f94f14a4fac51174f2f3a06c26da1
f0remangod <probill27@gmail.com>
2022-10-13 19:47:01 +0300

    vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const video = new Player('vimeo-player');
+const currentStorageValue = localStorage.getItem(STORAGE_KEY);
+
+if (currentStorageValue) {
+  video.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+}
+
+video.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
